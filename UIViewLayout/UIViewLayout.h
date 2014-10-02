f4fcfd2437eec86c1dd8db26b445516ffb242d46
Jason Brennan <i.jasonbrennan@gmail.com>
2014-10-02 13:07:52 -0400

    Initial Commit

---
+++ UIViewLayout/UIViewLayout.h
@@ -0,0 +1,19 @@
+//
+//  UIViewLayout.h
+//  UIViewLayout
+//
+//  Created by Jason Brennan on Oct-02-2014.
+//  Copyright (c) 2014 Hopscotch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIViewLayout.
+FOUNDATION_EXPORT double UIViewLayoutVersionNumber;
+
+//! Project version string for UIViewLayout.
+FOUNDATION_EXPORT const unsigned char UIViewLayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIViewLayout/PublicHeader.h>
+
+
